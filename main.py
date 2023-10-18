8f1455fa38798fd0b2ea21aaf84ab165468a12fa
GCipry3 <galbeazaciprian30@gmail.com>
2023-10-18 12:44:25 +0300

    Create a widget, and requirements.txt file

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
-def main():
-    print("Hello World")
+import sys
+import random
+from PySide6.QtCore import Qt, Slot
+from PySide6.QtWidgets import QApplication, QLabel, QWidget, QPushButton, QVBoxLayout
+                                                     
+class MyWidget(QWidget):
+    def __init__(self):
+        super().__init__()
 
-if __name__ == '__main__':
-    main()
+        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
+
+        self.button = QPushButton("Click me!")
+        self.text = QLabel("Hello World", alignment=Qt.AlignCenter)
+
+        self.layout = QVBoxLayout(self)
+        self.layout.addWidget(self.text)
+        self.layout.addWidget(self.button)
+
+        self.button.clicked.connect(self.magic)
+
+    @Slot()
+    def magic(self):
+        self.text.setText(random.choice(self.hello))
+
+if __name__ == "__main__":
+    app = QApplication([])
+
+    widget = MyWidget()
+    widget.resize(800, 600)
+    widget.show()
+
+    sys.exit(app.exec())
